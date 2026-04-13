fc34ad24b60307e3d12fc8cf86c60f6ef5a6b7a1
huseyindavut <saskin@protonmail.com>
2026-04-13 13:53:00 +0200

    Remove Hardenize dependency, simplify SSL checks to SSL Labs + direct TLS fallback.

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94a40e17878c0333e6aae84ae6c252cc88e14e0a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb603e9cb450142d757501c0d9ebaeda13434a34")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
